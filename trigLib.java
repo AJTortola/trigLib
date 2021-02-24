c4db91d39a52b2baa267f9b8f1862509e7ffda50
Anthony Tortola <57362052+AJTortola@users.noreply.github.com>
2021-02-23 20:40:50 -0500

    Update trigLib.java

--- trigLib.java
+++ trigLib.java
@@ -34,5 +34,47 @@ public class trigLib {
 	    
 	    }
 		
+		public static double cos(double x){ //cos function
+
+	       
+	        x = x % (2 * Pi); //Keeps the value in radians
+
+	        
+	        double value = 1.0;
+
+                double sum  = 1.0;      
+
+	        for (int i = 1; value != 0.0; i++) {
+	            value = value * (x / i);
+	            if (i % 4 == 2) 
+                        sum = sum - value;
+                    if (i % 4 == 0) 
+                        sum = sum + value;
+	        }
+
+	        return sum;
+	    
+	    }
+		
+		
+		public static double tan(double x){ //tan function
+	        
+	    }
+		
+		//converts degrees to radians
+		public static double DegSin(double x){
+
+	    }
+		
+                //converts degrees to radians
+		public static double DegCos(double x){
+
+	    }
+		
+                //converts degrees to radians
+		public static double DegTan(double x){
+
+	    }
+		
 	}
 }
